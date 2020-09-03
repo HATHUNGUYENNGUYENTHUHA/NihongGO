e6d4f15113578ed62e2fffbbe9aafe8e496f7bf2
thu ha <nguyenthuha110929@gmail.com>
2020-09-03 19:48:13 +0700

    add new projec

---
+++ src/com/vn/ui/nihonggo/NguPhap.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.ui.nihonggo;
+
+/**
+ *
+ * @author nguye
+ */
+public class NguPhap {
+    private String img;
+    private String phienAmHina;
+    private String nghe;//cho nay e se tao bien tu tao de luu, tam thoi de vay
+    private String mauCau;
+
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getPhienAm() {
+		return phienAmHina;
+	}
+
+	public void setMemberName(String phienAmHina) {
+		this.phienAmHina = phienAmHina;
+	}
+
+	public String getnghe() {
+		return nghe;
+	}
+
+	public void setNghe(String nghe) {
+		this.nghe = nghe;
+	}
+
+
+	public void setMauCau(String mauCau) {
+		this.img = img;
+	}
+
+	public void setIsActived(String mauCau) {
+		this.mauCau = mauCau;
+	}
+
+    
+}
